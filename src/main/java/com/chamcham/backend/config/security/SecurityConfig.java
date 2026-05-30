955e7f86dca2a178fa8f638dceee8cdc1c6e6abc
Sajjad Khan <sajjad.khan-c@progleasing.com>
2026-05-30 14:33:21 +0500

    new changes

--- src/main/java/com/chamcham/backend/config/security/SecurityConfig.java
+++ src/main/java/com/chamcham/backend/config/security/SecurityConfig.java
@@ -39,7 +39,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/send-otp", "/api/v1/auth/verify-otp", "/api/v1/auth/refresh", "/api/v1/auth/forgot-password", "/api/v1/auth/reset-password").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/creators", "/api/v1/brands").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/v1/packages/**", "/api/v1/creators/*/reviews").permitAll()
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/api/v1/packages",
+                                "/api/v1/packages/featured",
+                                "/api/v1/packages/*",
+                                "/api/v1/creators/*/reviews"
+                        ).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/creators", "/api/v1/creators/*", "/api/v1/brands",
                                 "/api/v1/brands/*")
                         .permitAll()
